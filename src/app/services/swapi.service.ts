1ccf368342296a69d55e8964844b00a7b2d6d45b
Wojciech Bubolka <bubolka@gmail.com>
2019-05-26 20:30:51 +0200

    swapi api

--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Films } from '../interfaces/films';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
-  constructor() { }
+  private baseUrl: string = 'https://swapi.co/api/';
+
+  constructor(
+    private api: ApiService
+  ) { }
+
+  async getFilms(): Promise<Films> {
+    try {
+      const url = await this.api.getUrl(this.baseUrl, 'films/' );
+      const respone = await this.api.get(url);
+      return respone;
+    } catch (e) {
+      console.log('error', e);
+    }
+  }
+  getFilm(id: string) {
+  }
+  getPeople() {
+
+  }
+  getPerson(id: string) {
+
+  }
+  getPlanets() {
+
+  }
+  getPlanete(id: string) {
+
+  }
+  getSpecies() {
+
+  }
+  getSpecie(id: string) {
+
+  }
+  getStarships() {
+
+  }
+  getStarship(id: string) {
+
+  }
+  getVehicles() {
+
+  }
+  getVehicle(id: string) {
+
+  }
 }
