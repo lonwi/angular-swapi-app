5b2b69a6317219236997aec7a8ff97d7590999a6
Wojciech Bubolka <bubolka@gmail.com>
2019-05-29 09:14:40 +0200

    pipes location

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IdFromUrlPipe } from './id-from-url.pipe';
-import { NewLinePipe } from './new-line.pipe';
+import { IdFromUrlPipe } from './id-from-url/id-from-url.pipe';
+import { NewLinePipe } from './new-line/new-line.pipe';
+import { SpeciePipe } from './specie/specie.pipe';
 
 @NgModule({
-  declarations: [IdFromUrlPipe, NewLinePipe],
+  declarations: [IdFromUrlPipe, NewLinePipe, SpeciePipe],
   imports: [
     CommonModule
   ],
-  exports: [IdFromUrlPipe, NewLinePipe]
+  exports: [IdFromUrlPipe, NewLinePipe, SpeciePipe]
 })
 export class PipesModule { }
---
+++ src/app/pipes/specie/specie.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SpeciePipe } from './specie.pipe';
+
+describe('SpeciePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SpeciePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
