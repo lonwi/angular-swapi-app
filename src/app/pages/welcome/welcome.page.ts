c5b4835716bcfc890c4b0ca0e6ff85378b3c8004
Wojciech Bubolka <bubolka@gmail.com>
2019-05-30 20:08:34 +0100

    add intro

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonContent) content: IonContent;
+
+  stars: Array<{ top: number, left: number }>;
+
+  constructor(
+    private platform: Platform
+  ) { }
 
   ngOnInit() {
+    this.getRandomStars().then(() => console.log(this.stars));
+  }
+
+  async getRandomStars(): Promise<Array<{ top: number, left: number }>> {
+    const stars = await Array(100).fill(0).map(() => {
+      const star = {
+        top: Math.floor(Math.random() * this.platform.height()),
+        left: Math.floor(Math.random() * this.platform.width())
+      };
+      return star;
+    });
+    this.stars = await stars;
+    return this.stars;
   }
 
 }
