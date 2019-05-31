90d70472a5b5cd46445130a0a5753d1a7209e9a3
Wojciech Bubolka <bubolka@gmail.com>
2019-05-31 20:49:12 +0100

    display data

--- src/app/pages/specie/specie.page.ts
+++ src/app/pages/specie/specie.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SwapiService } from 'src/app/services/swapi.service';
+
+import { FilmObject } from 'src/app/interfaces/film';
+import { SpecieObject } from 'src/app/interfaces/specie';
+import { PersonObject } from 'src/app/interfaces/person';
+import { PlanetObject } from 'src/app/interfaces/planet';
 
 @Component({
   selector: 'app-specie',
@@ -7,9 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeciePage implements OnInit {
 
-  constructor() { }
+  id: string;
+  specie: SpecieObject;
+
+  films: FilmObject[];
+  people: PersonObject[];
+  homeworld: PlanetObject;
+
+  constructor(
+    private route: ActivatedRoute,
+    private swapi: SwapiService,
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params.id;
+    this.loadData();
+  }
+
+  async loadData() {
+    this.specie = await this.swapi.getSpecie(this.id);
+
+    const films: FilmObject[] = [];
+    Promise.all(
+      this.specie.films.map(async (item) => {
+        const film = await this.swapi.get(item);
+        films.push(film);
+        return item;
+      })
+    ).then(() => this.films = films);
+
+    const people: PersonObject[] = [];
+    Promise.all(
+      this.specie.people.map(async (item) => {
+        const person = await this.swapi.get(item);
+        people.push(person);
+        return item;
+      })
+    ).then(() => this.people = people);
+
+    const homeworld: PlanetObject = await this.swapi.get(this.specie.homeworld);
+    this.homeworld = homeworld;
+
+    console.log(this.specie);
+    return this.specie;
+  }
+
+  isNumber(value: any): boolean {
+    return !isNaN(value) ? true : false;
   }
 
 }
