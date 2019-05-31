d7adef675327f45161032302773d8f7846f5884c
Wojciech Bubolka <bubolka@gmail.com>
2019-05-31 21:32:04 +0100

    display details

--- src/app/pages/starship/starship.page.ts
+++ src/app/pages/starship/starship.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SwapiService } from 'src/app/services/swapi.service';
+
+import { FilmObject } from 'src/app/interfaces/film';
+import { PersonObject } from 'src/app/interfaces/person';
+import { StarshipObject } from 'src/app/interfaces/starship';
+
 
 @Component({
   selector: 'app-starship',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarshipPage implements OnInit {
 
-  constructor() { }
+  id: string;
+  starship: StarshipObject;
+
+  films: FilmObject[];
+  people: PersonObject[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private swapi: SwapiService,
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params.id;
+    this.loadData();
+  }
+
+  async loadData() {
+    this.starship = await this.swapi.getStarship(this.id);
+
+    const films: FilmObject[] = [];
+    Promise.all(
+      this.starship.films.map(async (item) => {
+        const film = await this.swapi.get(item);
+        films.push(film);
+        return item;
+      })
+    ).then(() => this.films = films);
+
+    const people: PersonObject[] = [];
+    Promise.all(
+      this.starship.pilots.map(async (item) => {
+        const person = await this.swapi.get(item);
+        people.push(person);
+        return item;
+      })
+    ).then(() => this.people = people);
+
+    return this.starship;
+  }
+
+  isNumber(value: any): boolean {
+    return !isNaN(value) ? true : false;
   }
 
 }
--- src/app/pages/vehicle/vehicle.page.ts
+++ src/app/pages/vehicle/vehicle.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SwapiService } from 'src/app/services/swapi.service';
+
+import { FilmObject } from 'src/app/interfaces/film';
+import { PersonObject } from 'src/app/interfaces/person';
+import { VehicleObject } from 'src/app/interfaces/vehicle';
 
 @Component({
   selector: 'app-vehicle',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehiclePage implements OnInit {
 
-  constructor() { }
+  id: string;
+  vehicle: VehicleObject;
+
+  films: FilmObject[];
+  people: PersonObject[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private swapi: SwapiService,
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params.id;
+    this.loadData();
+  }
+
+  async loadData() {
+    this.vehicle = await this.swapi.getVehicle(this.id);
+
+    const films: FilmObject[] = [];
+    Promise.all(
+      this.vehicle.films.map(async (item) => {
+        const film = await this.swapi.get(item);
+        films.push(film);
+        return item;
+      })
+    ).then(() => this.films = films);
+
+    const people: PersonObject[] = [];
+    Promise.all(
+      this.vehicle.pilots.map(async (item) => {
+        const person = await this.swapi.get(item);
+        people.push(person);
+        return item;
+      })
+    ).then(() => this.people = people);
+    
+    console.log(this.vehicle);
+
+    return this.vehicle;
+  }
+
+  isNumber(value: any): boolean {
+    return !isNaN(value) ? true : false;
   }
 
 }
